c5a24dfe358e5520bf04ef1ea9da75972964a859
Jake Shelton <jakeshelton@Archies-Howl.local>
2019-03-25 16:27:04 -0600

    Mixed Data Types 4. Object-ception

--- js/app.js
+++ js/app.js
@@ -84,3 +84,27 @@ console.log(crazyObject.larry.characters[2].favourtieHobby);
 console.log(crazyObject.larry.nicknames[1]);
 // - The object the contains the name funkhauser
 console.log(crazyObject.larry.characters[1]);
+
+// 4. Object-ception
+// With the following object:
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt",
+                          },
+                       },
+                   },
+               },
+           },
+       },
+   },
+};
+// Change the value of limbo to null
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo);
+
